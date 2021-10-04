8c96b9862cedd1fd536b272c274c8bdea143caca
abdishakur rage <abdishakurrage06@gmail.com>
2021-10-04 02:12:42 -0500

    weekend-jquery-server-calculator-generate

--- server/server.js
+++ server/server.js
@@ -6,8 +6,17 @@ app.use(express.static('./server/public'));
 
 app.use(express.urlencoded({extended: true}));
 
+
+app.get('/calculate', (req,res) => {
+    console.log('sending data'); 
+    res.send(calculation);
+})
+
+
 // listening for requests
 const port = 5000;
 app.listen(port, () => {
     console.log('App is up and running!');
 });
+
+
