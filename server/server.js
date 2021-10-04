a175a2228d1a9081448555ea3fda478fcec90341
abdishakur rage <abdishakurrage06@gmail.com>
2021-10-03 20:24:04 -0500

    basic setup done

---
+++ server/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+// app.use 
+app.use(express.static('./server/public'));
+// app.use(bodyParser.json());
+app.use(express.urlencoded({extended: true}));
+
+// listening for requests
+const port = 5000;
+app.listen(port, () => {
+    console.log('App is up and running!');
+});
